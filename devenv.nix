50babc69f52945f14773d7c9011a677be5b0a130
Jason Riddle <contact+gh@jasonriddle.com>
2023-04-28 10:13:46 -0700

    Add devenv to root

---
+++ devenv.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/languages/
+  languages.python = {
+    enable = true;
+    venv.enable = true;
+  };
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.docker
+  ];
+
+  enterShell = ''
+    pip install ansible molecule[docker] docker
+  '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  pre-commit.hooks.shellcheck.enable = false;
+}
